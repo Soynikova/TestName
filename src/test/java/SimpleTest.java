e98014d7175bba08222bd9c6c60c3ee5a34c16a0
Soynikova <ekaterina.soinickova@yandex.ru>
2017-11-03 10:18:27 +0300

    edit simple test

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -23,24 +23,18 @@ public class SimpleTest {
     public void test() throws Exception{
        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tech\\Desktop\\chromedriver_win32\\chromedriver.exe");
        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://passport.yandex.ru/");
-        WebElement  login = driver.findElement(By.name("login"));
-        login.sendKeys("ekaterina.soinickova@yandex.ru");
-        WebElement pass =driver.findElement(By.name("passwd"));
-        pass.sendKeys("Soinikova1994");
-        WebElement button = driver.findElement(By.className("passport-Button-Text"));
-        button.click();
-        driver.get("https://mail.yandex.ru/");
+       driver.manage().window().maximize();
+       driver.get("https://passport.yandex.ru/");
+       WebElement  login = driver.findElement(By.name("login"));
+       login.sendKeys("ekaterina.soinickova@yandex.ru");
+       WebElement pass =driver.findElement(By.name("passwd"));
+       pass.sendKeys("Soinikova1994");
+       WebElement button = driver.findElement(By.className("passport-Button-Text"));
+       button.click();
+       driver.get("https://mail.yandex.ru/");
        Thread.sleep(4000);
-        WebElement element = driver.findElement(By.xpath("//*[@href='#compose']"));
-        element.click();
-        WebElement comu = driver.findElement(By.cssSelector(".js-compose-field"));
-        comu.sendKeys("831468@bsu.edu.ru");
-
-        comu.sendKeys("831468@bsu.edu.ru");
-       WebElement l = driver.findElement(By.cssSelector(".js-compose-field"));
-        l.sendKeys("831468@bsu.edu.ru");
-
+       WebElement element = driver.findElement(By.xpath("//*[@href='#compose']"));
+       element.click();
+       WebElement to = driver.findElement(By.xpath("//@type='hidden']"));
 
     }}
